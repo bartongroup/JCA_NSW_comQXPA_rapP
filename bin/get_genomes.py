290a12b40539c276187c00d5068e373486f0c09a
James Abbott <j.abbott@dundee.ac.uk>
2024-05-28 11:47:09 +0100

    Complete separation of sequences by gene/type and writing to output files

--- bin/get_genomes.py
+++ bin/get_genomes.py
@@ -1,15 +1,21 @@
 #!/bin/env python
 
 import gzip
+import re
 import requests
 
 from Bio import SeqIO
 from Bio.Seq import Seq
 from Bio.SeqRecord import SeqRecord
+from collections import ChainMap
 from json import loads
 from lxml import etree
 from pathlib import Path
 from tqdm import tqdm
+
+from pprint import pprint
+
+REQUIRED_GENES = ['comP']
 
 """
 Identifies all complete B.subtilis genomes available via ENA and downloads them
@@ -83,29 +89,42 @@ def is_complete(local_file):
         state(bool): True if assembly contains no gaps
     """
 
-    tree = etree.parse(local_file)
-
-    #  Gap info is stored within ASSEMBLY_ATTRIBUTES as follow:
-    #
-    #  <ASSEMBLY_ATTRIBUTE>
-    #   <TAG>spanned-gaps</TAG>
-    #   <VALUE>0</VALUE>
-    #  </ASSEMBLY_ATTRIBUTE>
-    #  <ASSEMBLY_ATTRIBUTE>
-    #    <TAG>unspanned-gaps</TAG>
-    #    <VALUE>0</VALUE>
-    #  </ASSEMBLY_ATTRIBUTE>
-    
-    tags = tree.xpath(".//ASSEMBLY_ATTRIBUTE/TAG[contains(text(),'spanned-gaps')]/following-sibling::VALUE")
-    spanned_gaps = tags[0].text
-
-    tags = tree.xpath(".//ASSEMBLY_ATTRIBUTE/TAG[contains(text(),'unspanned-gaps')]/following-sibling::VALUE")
-    unspanned_gaps = tags[0].text
-
     state = False
 
-    if spanned_gaps == '0' and unspanned_gaps == '0':
-        state = True
+    # set defaults which will not trigger detection if assembly attributes missing from record
+    spanned_gaps = 1
+    unspanned_gaps = 1
+
+    tree = etree.parse(local_file)
+
+    # Some accessions are returning as not found so these need to be handled
+    error = tree.xpath("/ErrorDetails/status")
+
+    if error:
+        print(f"{local_file} error status: {error[0].text}")
+    else:
+        #  Gap info is stored within ASSEMBLY_ATTRIBUTES as follow:
+        #
+        #  <ASSEMBLY_ATTRIBUTE>
+        #   <TAG>spanned-gaps</TAG>
+        #   <VALUE>0</VALUE>
+        #  </ASSEMBLY_ATTRIBUTE>
+        #  <ASSEMBLY_ATTRIBUTE>
+        #    <TAG>unspanned-gaps</TAG>
+        #    <VALUE>0</VALUE>
+        #  </ASSEMBLY_ATTRIBUTE>
+
+        tags = tree.xpath(".//ASSEMBLY_ATTRIBUTE/TAG[contains(text(),'spanned-gaps')]/following-sibling::VALUE")
+        if len(tags):
+            spanned_gaps = tags[0].text
+
+        tags = tree.xpath(".//ASSEMBLY_ATTRIBUTE/TAG[contains(text(),'unspanned-gaps')]/following-sibling::VALUE")
+        if len(tags):
+            unspanned_gaps = tags[0].text
+
+
+        if spanned_gaps == '0' and unspanned_gaps == '0':
+            state = True
 
     return(state)
 
@@ -146,49 +165,97 @@ def get_gene_sequences(local_genome):
                          as BioSeq objects, keyed on seq ID
     """
 
-    required_genes = ['comP']
     gene_seqs = dict()
     accession = str(local_genome.stem).replace('.embl','')
 
     with gzip.open(local_genome, 'rt') as fh:
         for record in SeqIO.parse(fh, format = 'embl'):
+            organism = record.annotations['organism']
             for feature in record.features:
 
                 # Handle genes by extracting subsequence from record
                 if feature.type == 'gene':
-                    if 'gene' in feature.qualifiers.keys() and feature.qualifiers.get('gene')[0] in required_genes:
+                    if 'gene' in feature.qualifiers.keys() and feature.qualifiers.get('gene')[0] in REQUIRED_GENES:
 
                         gene = feature.extract(record)
-                        id = f'{accession}_{feature.qualifiers.get('gene')[0]}_gene'
+                        gene_id = feature.qualifiers.get('gene')[0]
+
+                        id = f'{accession}_{gene_id}_gene'
+
                         gene.id = id
+                        gene.description = f"{organism} {gene_id} gene"
+
                         gene_seqs[id] = gene
 
                 # CDS feature contain the protein sequence as a 'translation' qualifier
                 elif feature.type == 'CDS':
 
-                    if 'gene' in feature.qualifiers.keys() and feature.qualifiers.get('gene')[0] in required_genes:
+                    if 'gene' in feature.qualifiers.keys() and feature.qualifiers.get('gene')[0] in REQUIRED_GENES:
                         if 'translation' in feature.qualifiers.keys():
 
+                            gene_id = feature.qualifiers.get('gene')[0]
                             prot_seq = feature.qualifiers.get('translation')[0]
-                            id = f'{accession}_{feature.qualifiers.get('gene')[0]}_protein'
-                            protein = SeqRecord(Seq(prot_seq), id = id)
+                            id = f'{accession}_{gene_id}_protein'
+
+                            protein = SeqRecord(Seq(prot_seq), 
+                                                id = id, 
+                                                description = f"{organism} {gene_id} protein")
 
                             gene_seqs[id] = protein
 
     return(gene_seqs)
 
+def write_outputs(all_seqs, output_dir):
+    """
+    Writes fasta formatted records for each gene/protein
+
+    Required params:
+        all_seqs(dict): dicts combing results returned from get_gene_sequences()
+        output_dir(pathlib.Path): Location to write results
+
+    Returns:
+        None
+    """
+
+    separated_seqs = dict()
+
+    for gene in REQUIRED_GENES:
+        for seq_id in all_seqs.keys():
+            record = all_seqs[seq_id]
+
+            if gene in seq_id:
+
+                match = re.search(r'_(gene|protein)$', seq_id)
+                if match:
+                    seq_type = match.group(1)
+                else:
+                    raise Exception (f'Failed to match sequence type: {seq_id}')
+
+                record.id = seq_id.replace(f"_{gene}_{seq_type}", "")
+                if f"{gene}_{seq_type}" in separated_seqs:
+                    separated_seqs[f"{gene}_{seq_type}"].append(record)
+                else:
+                    separated_seqs[f"{gene}_{seq_type}"] = [record]
+
+    for key in separated_seqs.keys():
+        gene,seq_type = key.split('_')
+        with open(f'outputs/{gene}.{seq_type}.fa', 'w') as fh:
+            SeqIO.write(separated_seqs[key], fh, 'fasta')
 
 def main():
 
-    xml_dir = Path(__file__).parents[1] / Path('xml')
+    xml_dir    = Path(__file__).parents[1] / Path('xml')
     genome_dir = Path(__file__).parents[1] / Path('genomes')
+    output_dir = Path(__file__).parents[1] / Path('outputs')
 
     xml_dir.mkdir(exist_ok = True)
     genome_dir.mkdir(exist_ok = True)
-
-    genome_info = search_available()
+    output_dir.mkdir(exist_ok = True)
 
     all_seqs = list()
+
+    genome_info = search_available()
+    complete_count = 0
 
     for assembly in tqdm(genome_info):
 
@@ -199,13 +266,19 @@ def main():
 
         download_assembly_xml(local_xml, accession)
         complete = is_complete(local_xml)
+        if complete:
+            complete_count += 1
 
         if complete:
             download_assembly(accession, local_genome)
             gene_seqs = get_gene_sequences(local_genome)
             all_seqs.append(gene_seqs)
 
-        break
+    all_seqs = dict(ChainMap(*all_seqs))
+
+    write_outputs(all_seqs, output_dir)
+
+    print(f'Complete genomes: {complete_count}')
 
 if __name__ == "__main__":
     main()
