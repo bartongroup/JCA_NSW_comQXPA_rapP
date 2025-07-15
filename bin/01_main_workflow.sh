ab47142dbe9a19cfb53d4a9959216b13e3f5a02e
James Abbott <j.abbott@dundee.ac.uk>
2025-07-15 11:16:26 +0100

    Cleanup commented code

--- bin/01_main_workflow.sh
+++ bin/01_main_workflow.sh
@@ -25,7 +25,3 @@ if [[ "${base_dir}" != "${analysis_root}" ]]; then
 fi
 
 snakemake --profile profile $@
-
-#snakemake  --snakefile workflow/Snakefile --rerun-incomplete --cluster-config config/workflow.json \
-#    --latency-wait 60 -j 50 --jn annotate_{jobid} --use-conda --conda-frontend mamba -k --drmaa \
-#	" -cwd -j y -o logs/{rule}.{wildcards}.log -jc {cluster.jc} -pe smp {cluster.threads} -mods l_hard mfree {cluster.mem_free} -adds l_hard local_free {cluster.local_free} " $@
