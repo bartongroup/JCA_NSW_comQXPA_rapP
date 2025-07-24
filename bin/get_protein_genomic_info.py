c9f755652bf83f52c55c9135928c8e553515ddcd
James Abbott <j.abbott@dundee.ac.uk>
2025-07-24 14:53:04 +0100

    Optionally output fasta protein sequences

--- bin/get_protein_genomic_info.py
+++ bin/get_protein_genomic_info.py
@@ -6,6 +6,7 @@ import gzip
 import json
 import re
 import sys
+from io import TextIOWrapper
 
 from Bio import SeqIO
 
@@ -106,6 +107,39 @@ def print_details(ids, data):
         else:
             print(tag)
 
+def print_fasta(ids, data, protein_dir, outfile):
+
+    """
+    Looks up and prints fasta-formatted sequence[s] for
+    provided identifiers
+
+    Required params:
+        ids(string): (posibly multiline) list of ids
+        data(dict): Dictionary keyed on locus tax prefix produced by parse_data()
+        protein_dir(pathlib.Path): Path to protein fasta directory
+        outfile (str): Path to output file
+    
+    Returns:
+        None
+    """
+
+    with open(outfile, 'w', encoding='UTF-8') as outfh:
+        if isinstance(ids, str):
+            ids= ids.split()
+
+        for locus_id in ids:
+
+            tag = locus_id.split('_')[0]
+            if tag in data:
+                accession = data[tag]['accession']
+
+                with open(Path(protein_dir / f"{accession}.fasta"), 'r', encoding='UTF-8') as fh:
+                    for record in SeqIO.parse(fh, 'fasta'):
+                        if locus_id in record.id:
+                            SeqIO.write([record], outfh, format='fasta')
+                            break
+
+
 def main():
 
     """
@@ -116,17 +150,21 @@ def main():
         prog = __file__,
             description = """
             Looks up genome/gene associated with a locus tagged identifier i.e.
-            HLBEOP_22245. If preparsed data is available (locus_tag_data.json)
+            HLBEOP_22245, or returns the appropriate fasta formatted sequence. 
+            If preparsed data is available (locus_tag_data.json)
             this will be used, otherwise the available genomes will be parsed, 
             and the results stored for reuse""" )
 
     parser.add_argument('-i', '--input', help="Idenfiers(s) to resolve")
+    parser.add_argument('-f', '--fasta', required=False, action='store_true', help="Return fasta formatted sequence")
+    parser.add_argument('-o', '--output', required=False, help='Path to write output fasta file')
     parser.add_argument('-j', '--json', help="Path to genome_locus_tags.json.gz file",
         default = 'data/full/genome_locus_tags.json.gz')
     args = parser.parse_args()
 
     serialised_data = Path(args.json)
     genome_dir = serialised_data.parent / 'annotations'
+    protein_dir = serialised_data.parent / 'fasta/proteins/'
 
     if serialised_data.exists():
 
@@ -145,7 +183,10 @@ def main():
     else:
         ids = sys.stdin.read()
 
-    print_details(ids, data)
+    if args.fasta:
+        print_fasta(ids, data, protein_dir, args.output)
+    else:
+        print_details(ids, data)
 
 if __name__ == "__main__":
     main()
