3ed588144684792893a62f133674573cace2e150
James Abbott <j.abbott@dundee.ac.uk>
2025-10-27 13:13:40 +0000

    Additional scripts for generating specific trees

---
+++ bin/05_3610LXG_phylogeny.sh
@@ -0,0 +1,30 @@
+#!/usr/bin/env bash
+
+#$ -pe smp 16
+#$ -jc rhel9
+#$ -j y
+#$ -o drmaa_logs/$JOB_NAME.$JOB_ID
+#$ -cwd
+#$ -N yeeF_phylogeny
+
+# Generates yeeF phylogeny 
+
+if [[ ${CONDA_DEFAULT_ENV} != "comQ_phylogeny" ]]; then
+    echo
+    echo "This script should be run under the 'comQ_phylogeny' conda environment"
+    echo
+    echo "The environment can be created with:"
+    echo
+    echo "     conda env create -f etc/comQ_phylogeny.yaml"
+    echo
+    echo "then activated with"
+    echo
+    echo "     conda activate comQ_phylogeny"
+    echo
+fi
+
+muscle -threads 16 -align NRSW_trees/3610LXG/3610_LXG.fasta -output NRSW_trees/3610LXG/aligned.fasta
+
+# iqtree run with modelfinder plus , and 1000 bootstrap iterations 
+iqtree -s NRSW_trees/3610LXG/aligned.fasta -m MFP -B 1000 
+
